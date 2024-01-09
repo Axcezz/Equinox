32c2f7500b3e8ca377f3aa8e29bc0851e18def25
Axcezz <alex_ander_90@hotmail.com>
2024-01-09 10:11:53 +0100

    updated menu and welcome page

--- app/game/Components/Menu.tsx
+++ app/game/Components/Menu.tsx
@@ -6,45 +6,39 @@ function Menu() {
     return (
 
 <div className="menu_simple">
-	<ul id="menu">
-		<h2 className="menuheader">Admin</h2>
 
-
-<li><a href='admin/index.php'>Admin panel</a></li>
-<li><a href='testpage.php' target='trigger'>Test page</a></li>
-<li><a href='todos.php' target='trigger'>Todos</a></li>
+<ul id="menu">
+<h2 className="menuheader">Admin</h2>
+	<li><Link href='admin/dashboard'>Admin panel</Link></li>
+	<li><Link href='admin/test'>Test page</Link></li>
+	<li><Link href='admin/todos'>Todos</Link></li>
 
 <h2 className="menuheader">General</h2>
 	<li><Link href="/game/gameforum">Game Forum</Link></li>
 	<li><Link href="/game">News & Updates</Link></li>
-	<li><a href="tos.php" target="trigger">Terms Of Service</a></li>
-	<li><a href="faq.php" target="trigger">FAQ's</a></li>
-	<li><a href="helpcenter.php" target="trigger">Support</a></li>
-	<li><a href="online.php" target="trigger" >Online</a></li>
-	<li><a href="general_stats.php" target="trigger">General Stats</a></li>
-	<li><a href="personal_stats.php" target="trigger" >Personal Stats</a></li>
-
+	<li><Link href="/game/tos">Terms Of Service</Link></li>
+	<li><Link href="/game/faq">FAQs</Link></li>
+	<li><Link href="/game/support">Support</Link></li>
+	<li><Link href="/game/online">Online</Link></li>
+	<li><Link href="/game/general-stats">General Stats</Link></li>
+	<li><Link href="/game/personal-stats">Personal Stats</Link></li>
 
 <h2 className="menuheader">Actions</h2>
-
-	<li><a href="mission.php" target="trigger">Mission</a></li>
-	<li><a href="university.php" target="trigger">University</a></li>
-	<li><a href="research.php" target="trigger">Research</a></li>
-	<li><a href="work.php" target="trigger">Work</a></li>
-	<li><a href="shop.php" target="trigger">Shop</a></li>
-	<li><a href="hospital.php" target="trigger">Hospital</a></li>
-	<li><a href="hengar.php" target="trigger">Hangar</a></li>
-	<li><a href="properties.php" target="trigger" >Properties</a></li>
-	<li><a href="credit_transfer.php" target="trigger" >Credit Transfer</a></li>
-	<li><a href="bank.php" target="trigger" >Bank</a></li>
-	<li><a href="casino.php" target="trigger" >Casino</a></li>
-	<li><a href="explore.php" target="trigger">Explore</a></li>
+	<li><Link href="/game/explore">Explore</Link></li>	
+	<li><Link href="/game/mission">Mission</Link></li>
+	<li><Link href="/game/university">University</Link></li>
+	<li><Link href="/game/research">Research</Link></li>
+	<li><Link href="/game/work">Work</Link></li>
+	<li><Link href="/game/shop">Shop</Link></li>
+	<li><Link href="/game/hospital">Hospital</Link></li>
+	<li><Link href="/game/hangar">Hangar</Link></li>
+	<li><Link href="/game/properties">Properties</Link></li>
+	<li><Link href="/game/bank">Bank</Link></li>
+	<li><Link href="/game/casino">Casino</Link></li>
 
 <h2 className="menuheader">Federation</h2>
-
-	<li><a href="/" target="trigger">Command Center</a></li>
-	<li><a href="logout.php">Log Out</a></li>
-
+	<li><Link href="/">Command Center</Link></li>
+	<li><Link href="logout.tsx">Log Out</Link></li>
 </ul>
 
 </div>
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -11,9 +11,17 @@ export default function Home() {
   return (
 
          
-<div className="flex justify-center items-center pt-28 flex-col">
-
-<Card title="Welcome">       
+<div className="flex justify-center items-center flex-col ">
+<Card title="Patch Note" subTitle="version 0.1" className="w-3/4 mt-4">       
+Several changes have been made to the game, here are the most important ones.
+<br />
+- Added the layout for the game forum.
+<br />
+- Added icons and ores for the userpanel.
+<br />
+- 
+</Card>
+<Card title="Welcome" className="w-3/4 mt-4">       
 Welcome to Equinox, a game where you can explore the universe and discover new planets, stars and galaxies.
 </Card>
 
