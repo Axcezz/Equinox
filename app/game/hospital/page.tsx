a7325dde600c08876dffef402fbf342e8a627ee3
Axcezz <alex_ander_90@hotmail.com>
2024-01-15 10:51:29 +0100

    added some comments and property owner

--- app/game/hospital/page.tsx
+++ app/game/hospital/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "primereact/button";
 import { InputNumber } from 'primereact/inputnumber';
 import HospitalImg from '/public/images/hospital.jpg'
 import Image from 'next/image';
+import Link from "next/link";
 
 export default function Hospital() {
 
@@ -28,27 +29,32 @@ Price and health should be balanced.
     );
     return (
         <div className="card flex justify-content-center">
-            <Card title="Hospital"className="w-1/3" header={header} subTitle="While in hospital, you cannot do other activities. you can not be attacked or attack other players. Choose the amount of hours you want health care for.">
-            <div className="flex flex-column gap-3 ">
-                {categories.map((category) => {
-                    return (
-                        
-                        <div key={category.key} className="flex align-items-center w-full">
-                           
-                            <RadioButton inputId={category.key} name="category" value={category} onChange={(e) => setSelectedCategory(e.value)} checked={selectedCategory.key === category.key} />
-                            <label htmlFor={category.key} className="ml-2 w-5/6">{category.name}</label>
-                            <label htmlFor={category.key} className="ml-2 w-2/6 flex items-center">
-                                <span>{category.price}</span>
-                            </label>
-                            <InputNumber inputId="minmax-buttons" size={1} mode="decimal" showButtons min={0} max={100} />
-                            <Button className="mx-5 w-1/6">Buy</Button>
-                        </div>
-                        
-                    );
-                })}
-            
+            <Card title="Hospital" className="w-1/3" header={header} subTitle="While in hospital, you cannot do other activities. you can not be attacked or attack other players. Choose the amount of hours you want health care for.">
+                <div className="flex flex-column gap-3 ">
+                    {categories.map((category) => {
+                        return (
+                            <div key={category.key} className="flex align-items-center w-full">
+                                <RadioButton inputId={category.key} name="category" value={category} onChange={(e) => setSelectedCategory(e.value)} checked={selectedCategory.key === category.key} />
+                                <label htmlFor={category.key} className="ml-2 w-5/6">{category.name}</label>
+                                <label htmlFor={category.key} className="ml-2 w-2/6 flex items-center">
+                                    <span>{category.price}</span>
+                                </label>
+                                <InputNumber inputId="minmax-buttons" size={1} mode="decimal" showButtons min={0} max={100} />
+                                <Button className="mx-5 w-1/6">Buy</Button>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="pt-5 justify-content-center flex">
+                Property is owned by <Link href="/" className="pl-1">Axcezz</Link>
+                    {/* 
+If there is no owner, the user can buy the property for a defined price.
+The property will be added to the users property list.
+The property will generate income based on users who use the hopital.
+*/}
             </div>
             </Card>
+
         </div>
     );
 }
