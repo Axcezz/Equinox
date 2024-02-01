5591f92a7c0fac447b369c3ff4ab7921120d5356
Axcezz <alex_ander_90@hotmail.com>
2024-02-01 16:43:02 +0100

    didnt manage to create the solar system in 3d so im just doing it a easy way out

--- app/game/explore/page.tsx
+++ app/game/explore/page.tsx
@@ -1,13 +1,43 @@
-import dynamic from 'next/dynamic';
+'use client'
+import { useState } from 'react';
+import { Card } from 'primereact/card';
+import Image from 'next/image';
 
-import SolarSystem from './components/SolarSystem';
+const solarSystems = [
+  { name: 'Solar System 1', planets: ['Earth', 'Mars'] },
+  { name: 'Solar System 2', planets: ['Venus', 'Jupiter'] },
+  // Add more solar systems as needed
+];
 
-const SolarSystemDynamic = dynamic(() => import('./components/SolarSystem'), { ssr: false });
+export default function Explore() {
+  const [selectedSolarSystem, setSelectedSolarSystem] = useState(null);
 
-export default function Home() {
+  const handleSolarSystemClick = (solarSystem) => {
+    setSelectedSolarSystem(solarSystem);
+  };
+
   return (
-    <div>
-      <SolarSystem />
+    <div className="flex justify-between w-3/4 mx-auto">
+      <div className="w-1/2">
+        {selectedSolarSystem && (
+          <>
+            <Image src="/images/planets/sun.png" alt="Sun" width={200} height={200} />
+            {selectedSolarSystem.planets.map((planet, index) => (
+              <div key={index}>
+                <Image src={`/images/planets/${planet}.png`} alt={planet} width={50} height={50} />
+                <span>{planet}</span>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+      <div className="w-1/2">
+        {solarSystems.map((solarSystem, index) => (
+          <Card key={index} title={solarSystem.name} onClick={() => handleSolarSystemClick(solarSystem)}>
+            <span>Click to view planets</span>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
